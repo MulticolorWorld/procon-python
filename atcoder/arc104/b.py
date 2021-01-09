2b7096616f4b83ee6e519504bf78f268a7fcee23
MulticolorWorld <multicolorworld.shinku@gmail.com>
2021-01-10 02:54:50 +0900

    精進

---
+++ atcoder/arc104/b.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    N, S = input().split()
+    N = int(N)
+    ans = 0
+    for i in range(N):
+        c1 = 0
+        c2 = 0
+        for j in range(i, N):
+            if S[j] == "A":
+                c1 += 1
+            elif S[j] == "T":
+                c1 -= 1
+            elif S[j] == "C":
+                c2 += 1
+            else:
+                c2 -= 1
+            if c1 == 0 and c2 == 0:
+                ans += 1
+    print(ans)
